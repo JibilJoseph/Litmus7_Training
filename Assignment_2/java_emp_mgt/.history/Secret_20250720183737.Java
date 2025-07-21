ffb86ecbd910b4e81b1149b0c574841d64a84bac
JibilJoseph <jibiljoseph2003@gmail.com>
2025-07-21 19:05:01 +0530

    A2.1

--- Assignment_2/java_emp_mgt/.history/Secret_20250720183737.Java
+++
@@ -1,5 +0,0 @@
-public class Secret {
-    public static final String JDBC_URL = "jdbc:mysql://localhost:3306/java";
-    public static final String DB_USER = "root";
-    public static final String DB_PASSWORD = "root";
-}
